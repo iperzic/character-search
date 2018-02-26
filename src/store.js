e86f160280c0a398f5ff0fb4ba32a30ab173c096
Igor Perzic <iperzic@yahoo.com>
2018-02-26 23:18:39 +0100

    Start using app epic

--- src/store.js
+++ src/store.js
@@ -1,6 +1,12 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createEpicMiddleware } from 'redux-observable';
+
 import appReducer from './reducers/app';
+import { searchCharacterEpic } from './epics/app';
+
+const epicMiddleware = createEpicMiddleware(searchCharacterEpic);
 
 export default createStore(
   appReducer,
+  applyMiddleware(epicMiddleware)
 );
