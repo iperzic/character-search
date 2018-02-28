4de17e328aeacd93a5d83d78842b77afc85a5b50
Igor Perzic <iperzic@yahoo.com>
2018-02-28 20:55:08 +0100

    Add app container component test

--- src/containers/App.test.js
+++ src/containers/App.test.js
@@ -1,9 +1,59 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import configureStore from 'redux-mock-store';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import App from './App';
+import testStore from '../test-support/testStore';
+
+describe('App', () => {
+  let store;
+
+  describe('when the there is no search query', () => {
+
+    describe('and there is no bookmarks', () => {
+      beforeEach(() => {
+        store = configureStore()(testStore.createFreshStore());
+      });
+
+      it('should show empty search input and empty grid', () => {
+        const appContainer = shallow(<App store={store} />).dive();
+
+        expect(appContainer.getElement()).toMatchSnapshot();
+      });
+    });
+
+    describe('and bookmarks exist', () => {
+      beforeEach(() => {
+        const id = 123;
+        const byId = { [id]: { id, name: 'Deadpool', image: 'some-image.jpg' } };
+        const characters = testStore.createCharacterStore(byId, undefined, [id]);
+        const metadata = testStore.createMetadataStore();
+        const app = testStore.createAppStore();
+        store = configureStore()({ characters, metadata, app });
+      });
+
+      it('should show empty search input and grid with bookmarks', () => {
+        const appContainer = shallow(<App store={store} />).dive();
+
+        expect(appContainer.getElement()).toMatchSnapshot();
+      });
+    });
+  });
+
+  describe('when the search query is set', () => {
+    beforeEach(() => {
+      const id = 123;
+      const byId = { [id]: { id, name: 'Deadpool', image: 'some-image.jpg' } };
+      const characters = testStore.createCharacterStore(byId, [id]);
+      const metadata = testStore.createMetadataStore();
+      const app = testStore.createAppStore('Deadpool');
+      store = configureStore()({ characters, metadata, app });
+    });
+
+    it('should show search input and grid with fetched characters', () => {
+      const appContainer = shallow(<App store={store} />).dive();
+
+      expect(appContainer.getElement()).toMatchSnapshot();
+    });
+  })
 });
